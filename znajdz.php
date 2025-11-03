78e7d14bd6bb0f06def7356fb758eeb812d9bbae
piotrglowka-collab <piotr.glowka@zsbem.edu.pl>
2025-11-03 10:30:15 +0100

    Add files via upload

---
+++ znajdz.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Kwiaty</title>
+    <link rel="stylesheet" href="styl3.css">
+</head>
+<body>
+    <header><h1>Grupa Polskich Kwiaciarni</h1></header>
+    <aside class="left"><h2>Menu</h2>
+    <ol>
+        <li><a href="index.html">Strona Główna</a></li>
+        <li><a href="https://www.kwiaty.pl/" target="_blank">Rozpoznaj kwiaty</a></li>
+        <li><a href="znajdz.php">Znajdź kwiaciarnię</a></li>
+          <ul>
+            <li>w Warszawie</li>
+            <li>w Malborku</li>
+            <li>w Poznaniu</li>
+          </ul>
+    </ol>        
+    </aside>
+    <aside class="right">
+        <h2>Znajdź kwiaciarnię</h2>
+    <form action="" method="post">
+        Podaj nazwę miasta:<input type="text"><button>SPRAWDŹ<button>
+    </form>
+    </aside>
+    <footer><p>Strone opracował: 00000000000</p></footer>
+</body>
+<?php
+mysqli_connect(localhost, root, , kwiaciarnia);
+?>
+</html>
+
+
